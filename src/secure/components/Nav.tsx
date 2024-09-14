3f02b5f1c7422ab9703c37e11576ba60309adb5c
Arcylisz <olek1305@gmail.com>
2024-09-14 13:34:18 +0200

    Fix problem CI/CD with href, change <a> to <button>

--- src/secure/components/Nav.tsx
+++ src/secure/components/Nav.tsx
@@ -22,7 +22,7 @@ class Nav extends Component {
         }
         return (
             <nav className="navbar navbar-dark sticky-top bg-dark flex-md-nowrap p-0 shadow">
-                <a className="navbar-brand col-md-3 col-lg-2 mr-0 px-3" href="#">Company name</a>
+                <button className="navbar-brand col-md-3 col-lg-2 mr-0 px-3" >Company name</button>
 
                 <ul className="navbar-nav px-3">
                     <li className="nav-item text-nowrap">
